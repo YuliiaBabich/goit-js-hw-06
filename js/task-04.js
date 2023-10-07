d167eae6d376f86581fdcf17a91e83861db69cbb
YuliiaBabich <aim2002@ukr.net>
2023-10-07 13:21:34 +0300

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,8 @@
+const buttons = document.querySelectorAll('button');
+console.log(buttons.values);
+
+
+//const counterValue = 
+ //   return counterValue 
+//type = "button" data-action="decrement"
+//type="button" data-action="increment"
