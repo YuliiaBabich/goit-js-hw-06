13f3e11b0f79a6d410b3c2f574e1f747ca23e6f2
YuliiaBabich <aim2002@ukr.net>
2023-10-08 12:56:54 +0300

    dz 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+//document.body.style.backgroundColor = '#ccdbd4';
+document.body.style.color = '#4CAF50';
+document.querySelector('button').style.backgroundColor = '#4CAF50'
+document.querySelector('button').style.color = 'white'
+
+const imputForm = document.querySelector('.login-form');
+//console.log(imputForm); 
+imputForm.addEventListener('submit', imputSubmit); //отклик на подію submit ввдення даних
+const subscriberData = {};// створюю пустий об'єкт куди буд записуватись введені дані
+
+ function imputSubmit (event) {
+   event.preventDefault(); // щоб сторінка не перезавантажувалася
+  const email = event.currentTarget.elements.email;
+  const password = event.currentTarget.elements.password;
+     if (email.value === '' || password.value === '') {  
+     alert('Please fill out all fields!');         //якщо одна із Форм пуста буде повідомлення
+   } else {
+    subscriberData.email = email.value;
+     subscriberData.password = password.value;     // в іншому випадку дані запишуться в обєкт 
+
+     console.log(subscriberData);
+   }
+  event.currentTarget.reset();   // перезавантажуеємо/онолюємо
+}
 
